d40d489c74928b5fd33211de813acd764fceb1b4
soyaib-shanto <134508875+soyaib-shanto@users.noreply.github.com>
2025-07-23 23:39:30 +0600

    Update User.java

--- src/main/java/com/example/SpringBootIntern/Model/User.java
+++ src/main/java/com/example/SpringBootIntern/Model/User.java
@@ -20,7 +20,7 @@ public class User {
 
     @Column(nullable = false)
     @NotBlank(message = "Password must not be empty")
-    @Size(min = 6, max = 30, message = "Password must be between 6 and 30 characters")
+   // @Size(min = 6, max = 30, message = "Password must be between 6 and 30 characters")
      private String password;
 
      @Column(nullable = false)
